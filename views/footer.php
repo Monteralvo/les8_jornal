ff308592f97421710a6678a162075496eee31c39
Monti <RNA2204@gmail.com>
2024-11-18 00:12:50 +0300

    Edit footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,8 @@
 <footer>
   <div class="navigation">
-    <a class='navigation__link' href=<?php "'".BASE_URI."'" ?>>Новости</a>
-    <a class='navigation__link' href=<?php "'".BASE_URI."'" ?>>Комментарии</a>
-    <a class='navigation__link' href=<?php "'".BASE_URI."'" ?>>Оставить отзыв</a>
-    <a class='navigation__link' href=<?php "'".BASE_URI."'" ?>>Об авторах</a>
+    <a class='navigation__link' href="news">Новости</a>
+    <a class='navigation__link' href="comments">Комментарии</a>
+    <a class='navigation__link' href="feedback">Оставить отзыв</a>
+    <a class='navigation__link' href="about">Об авторах</a>
   </div>
 </footer>
